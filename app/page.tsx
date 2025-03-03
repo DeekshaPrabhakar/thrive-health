9dbdd2b10664b92248c15a2e782050f8d16f381b
Deeksha Prabhakar <deekiyengar@gmail.com>
2025-03-02 16:02:17 -0800

    chore: tailwind theme and usage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from '../components/ui/button';
 
 export default function Home() {
   return (
-   <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      <Button className="text-3xl bg-pink-200 text-pink-900">Click me</Button>
+    </>
   );
 }
